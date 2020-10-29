d4085d31f9b54e5f1af48b6ab3cb87029705f9ce
John Hunt <john@matraining.com>
2020-10-29 17:28:51 +0000

    Added some global 'constants' for important values

--- 02-iteration-and-control/number_guess_game.py
+++ 02-iteration-and-control/number_guess_game.py
@@ -1,22 +1,26 @@
 import random
 
+MIN_VALUE = 1
+MAX_VALUE = 10
+MAX_NUMBER_OF_GUESSES = 4
+
 print('Welcome to the number guess game')
 
 # Initialise the number to be guessed
-number_to_guess = random.randint(1,10)
+number_to_guess = random.randint(MIN_VALUE, MAX_VALUE)
 
 # Initialise the number of tries the player has made
 count_number_of_tries = 1
 
 # Obtain their initial guess
-guess = int(input('Please guess a number between 1 and 10: '))
+guess = int(input('Please guess a number between ' + MIN_VALUE + ' and ' + MAX_VALUE + ': '))
 while number_to_guess != guess:
     print('Sorry wrong number')
 
     # Check to see they have not exceeded the maximum
     # number of attempts if so break out of loop otherwise
     # give the user come feedback
-    if count_number_of_tries == 4:
+    if count_number_of_tries == MAX_NUMBER_OF_GUESSES:
         break
     elif guess < number_to_guess:
         print('Your guess was lower than the number')
@@ -30,7 +34,7 @@ while number_to_guess != guess:
 # Check to see if they did guess the correct number
 if number_to_guess == guess:
     print('Well done you won!')
-    print('You took', count_number_of_tries , 'goes to complete the game')
+    print('You took', count_number_of_tries, 'goes to complete the game')
 else:
     print("Sorry - you loose")
     print('The number you needed to guess was',
